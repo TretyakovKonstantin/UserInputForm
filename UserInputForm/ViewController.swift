f402c0573231924feb64bfa0c7d90ab48cedc6e5
frame <apps@studiomobile.ru>
2017-11-04 22:36:30 +0300

    Initial Commit

---
+++ UserInputForm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserInputForm
+//
+//  Created by User on 04/11/2017.
+//  Copyright © 2017 Studio Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
