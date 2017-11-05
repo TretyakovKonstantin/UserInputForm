87097043245fd852c46c320f248c35cc9ae398a0
frame <user@Users-MacBook-Pro.local>
2017-11-05 13:13:29 +0300

    ínitial commit

--- UserInputForm/ViewController.swift
+++ UserInputForm/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
+let reuseIdentifier = "reuse identifier"
+
+class User {
+    
+}
+
 class ViewController: UIViewController {
+    
+    var viewAsTable: UITableView {
+        get {
+            return view as! UITableView
+        }
+    }
+    
+    override func loadView() {
+        view = UITableView()
+        view.backgroundColor = .white
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        viewAsTable.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        viewAsTable.dataSource = self
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
 }
 
