f262e1ba35ed2c67a16fb42590ec0de760d32200
sm-user <user@mac-mini-sm-user.local>
2017-11-10 23:36:14 +0300

    Template for MVVC created

---
+++ UserInputForm/Model/SerializeService.swift
@@ -0,0 +1,9 @@
+//
+//  SerializeService.swift
+//  UserInputForm
+//
+//  Created by sm-user on 10.11.2017.
+//  Copyright © 2017 Studio Mobile. All rights reserved.
+//
+
+import Foundation
---
+++ UserInputForm/Model/ViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModel.swift
+//  UserInputForm
+//
+//  Created by sm-user on 10.11.2017.
+//  Copyright © 2017 Studio Mobile. All rights reserved.
+//
+
+import Foundation
+
+func getData()->[User] {
+    return []
+}
